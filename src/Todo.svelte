a6108935263c29fc9eba6b2d8495df7fd13b6977
Andre Viallon <viallon.andre@gmail.com>
2021-08-12 17:55:26 +0200

    chore: todo component

---
+++ src/Todo.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+  
+	const dispatch = createEventDispatcher();
+  export let todo;
+
+	const deleteTodo = () => dispatch('delete');
+</script>
+
+<li>
+  {todo}
+  <button on:click={deleteTodo}>
+    Remove
+  </button>
+</li>
+
+<style>
+  li {
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
